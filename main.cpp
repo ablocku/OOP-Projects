b67fad1768530fa5910f1ec9db99bbdbd4a803d6
ablocku <45084824+ablocku@users.noreply.github.com>
2020-03-11 13:21:17 +0200

    Modificari Tema 1
    - am adaugat citirea din fisier, voi adauga in repository fisierul de intrare, alaturi de fisierul de iesire care ar trebui sa rezulte
    - am sters constructorul cu care se atribuie pe fiecare pozitie o valoare
    - metodele sort(int, int) si partitionare(int, int) au devenit private, de la public, iar pentru sortarea vectorului a aparaut metoda void sort(){ sort (0, n-1); }
    - am reparat indentarea care avea o problema de la linia 66
    - am adaugat spatiu dupa virgula la linia 144
    -am modificat functia de calculare a produsului scalar, astfel incat sa nu duplic codul (acum merg pana la min(n, v2.n) )
    -am modificat modul de afisare a vectorului:
    n
    [v1,v2,v3,...,vn]
    -nu mai afisez elementele pentru care s-a apelat constructorul simplu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+ifstream f("date.in");
+ofstream g("date.out");
 
 struct tuplu
 {
@@ -11,9 +14,16 @@ int maxi, poz;
 class Vector{
     int *v;
     int n;
-public:
 
-    Vector(int n1, int x);
+
+
+public:
+    void sort()
+    {
+        sort(0,n-1);
+    }
+
+    Vector(int n1, int x = 0);
     Vector();
     Vector(const Vector &v2);
     ~Vector();
@@ -21,22 +31,50 @@ public:
     void update(int n1, int x);
     long long sum();
     tuplu max();
-    void sort(int p, int q);
-    int partitionare(int p, int q);
     int operator * (Vector const &v2);
     friend istream &operator >>(istream &input,Vector &v1);
     friend ostream &operator <<(ostream &output, Vector v1);
     int len();
+
+private:
+    void sort(int p, int q)
+    {
+    int k = partitionare(p,q);
+    if (p<k-1)
+        sort(p,k-1);
+    if(k+1<q)
+        sort(k+1,q);
+    }
+
+    int partitionare(int p, int q)
+    {
+    int x = v[p];
+    int i,j;
+    i = p+1;
+    j = q;
+    while(i<=j)
+        {
+        if(v[i] <= x)
+            i++;
+        if(v[j] >= x)
+            j--;
+        if(v[i] > x and v[j] < x and i < j)
+            {
+            int aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            i++; j--;
+            }
+        }
+    v[p] = v[j];
+    v[j] = x;
+    return j;
+    }
+
+
 };
 
 
-Vector::Vector(int n1, int x)
-{
-    n = n1;
-    v = new int[n];
-    for(int i=0; i < n; ++i)
-        v[i] = x;
-}
 
 Vector::Vector()
 {
@@ -63,92 +101,60 @@ Vector::~Vector()
 
 void Vector::operator = (Vector const &v2)
 {
-delete []v;
-n = v2.n;
-v = new int[n];
-int i;
-for(i = 0; i < n; ++i)
-    v[i] = v2.v[i];
+    delete []v;
+    n = v2.n;
+    v = new int[n];
+    int i;
+    for(i = 0; i < n; ++i)
+        v[i] = v2.v[i];
 }
 
 void Vector::update(int n1, int x)
 {
-delete []v;
-n = n1;
-int i;
-v = new int[n];
-for(i = 0 ;i < n; ++i)
-    v[i] = x;
+    delete []v;
+    n = n1;
+    int i;
+    v = new int[n];
+    for(i = 0 ;i < n; ++i)
+        v[i] = x;
 }
 
 long long Vector::sum()
 {
-int i;
-long long s=0;
-for(i = 0; i < n; ++i)
-    s+= v[i];
-return s;
+    int i;
+    long long s=0;
+    for(i = 0; i < n; ++i)
+        s+= v[i];
+    return s;
 }
 
 tuplu Vector::max()
 {
-tuplu rez;
-rez.maxi = v[0];
-rez.poz = 0;
-int i;
-for(i = 1; i < n; ++i)
-    if(v[i]>rez.maxi)
-        {
-        rez.maxi = v[i];
-        rez.poz = i;
-        }
-return rez;
-}
-
-void Vector::sort(int p, int q)
-{
-int k = partitionare(p,q);
-if (p<k-1)
-    sort(p,k-1);
-if(k+1<q)
-    sort(k+1,q);
-}
-
-int Vector::partitionare(int p, int q)
-{
-int x = v[p];
-int i,j;
-i = p+1;
-j = q;
-while(i<=j)
-    {
-    if(v[i] <= x)
-        i++;
-    if(v[j] >= x)
-        j--;
-    if(v[i] > x and v[j] < x and i < j)
-        {
-        int aux = v[i];
-        v[i] = v[j];
-        v[j] = aux;
-        i++; j--;
-        }
-    }
-v[p] = v[j];
-v[j] = x;
-return j;
+    tuplu rez;
+    rez.maxi = v[0];
+    rez.poz = 0;
+    int i;
+    for(i = 1; i < n; ++i)
+        if(v[i]>rez.maxi)
+            {
+            rez.maxi = v[i];
+            rez.poz = i;
+            }
+    return rez;
 }
 
 int Vector::operator * (Vector const &v2)
 {
-int p = 0,i;
-if(n > v2.n)
-    for(i = 0; i < v2.n; ++i)
-        p += v[i] * v2.v[i];
-else
-    for(i = 0;i < n; ++i)
-        p += v[i]*v2.v[i];
-return p;
+    int p = 0, i;
+    int mini;
+    if(n > v2.n)
+        mini = v2.n;
+    else
+        mini = n;
+    for(i = 0; i < mini; ++i)
+            p += v[i] * v2.v[i];
+
+    return p;
 }
 
 istream& operator >>(istream &input, Vector &v1)
@@ -164,8 +170,10 @@ ostream& operator <<(ostream &output, Vector v1)
 {
     output<<v1.n<<'\n';
     int i;
-    for(i = 0; i < v1.n; ++i)
-        output<< v1.v[i]<<' ';
+    output<<'[';
+    for(i = 0; i < v1.n - 1; ++i)
+        output<< v1.v[i]<<',';
+    output<<v1.v[v1.n-1]<<']';
     output<<'\n';
     return output;
 }
@@ -177,9 +185,7 @@ int Vector::len()
 
 void runTests()
 {
-cout<<"Constructor de initializare cu o valoare:"<<'\n';
-Vector v2(3,5);
-cout<<v2<<'\n';
+Vector v2;
 cout<<"Constructor de copiere:"<<'\n';
 Vector v3 = v2;
 cout<<v3<<'\n';
@@ -193,24 +199,26 @@ cout<<v2<<'\n';
 cout<<"Metoda update: "<<'\n';
 v2.update(5,5);
 cout<<v2<<'\n';
-cout<<"Metoda max pt aflarea maximului si pozitiei lui -cititi un vector mai variat,urmeaza si sortarea acestuia-:"<<'\n';
+cout<<"Metoda max pt aflarea maximului si pozitiei lui :"<<'\n';
 cin>>v2;
 tuplu rez = v2.max();
-cout<<rez.maxi << ' ' << rez.poz<<'\n';
+cout<<v2;
+cout<<"Maximul este: "<<rez.maxi << " pe pozitia: " << rez.poz<<'\n'<<'\n';;
 cout<<"Metoda pt aflarea sumei elementelor vectorului:"<<'\n';
-cout<<v2.sum()<<'\n';
-v2.sort(0,v2.len()-1);
+cout<<v2.sum()<<'\n'<<'\n';;
+cout<<"Metoda pentru sortarea vectorului: "<<'\n';
+v2.sort();
 cout<<v2<<'\n';
 cout<<"Supraincarcarea lui '*' pt aflarea produsului scalar a 2 vectori"<<'\n';
-cout<<v3*v2<<'\n';
+cout<<v3*v2<<'\n'<<'\n';
 cout<<"Constructor pt initializare cu 0 pe toate pozitiile de la 0 la n-1 cu n = 10005:" << '\n';
-Vector v1;
-cout<<v1;
 }
 
 
 int main()
 {
+freopen("date.in","r",stdin);
+freopen("date.out","w",stdout);
 runTests();
 return 0;
 }
